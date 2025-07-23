cf02a39865c549d1bbf264ba0754e7697d37cf90
elpir <ripleysinclair@proton.me>
2025-07-22 22:26:45 -0500

    Made it so whenever you download a new spectrum it replaces the one in /data. Also, created a requirements.txt that might change later.

--- src/redshift_cc_sdss.py
+++ src/redshift_cc_sdss.py
@@ -1,8 +1,9 @@
 from astropy.io import fits
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 from astroquery.sdss import SDSS
-import redshift_cross_correlation as redshift_cross_correlation
+import os
+#import redshift_cross_correlation as redshift_cross_correlation
 
 
 def get_spectrum(plate=2663, mjd=54234, fiberID=332):
@@ -14,37 +15,34 @@ def get_spectrum(plate=2663, mjd=54234, fiberID=332):
     
     if spectra:
         print("Spectrum retrieved!")
-        return get_flux_and_wl(spectra)
+        spectrum = get_flux_and_wl(spectra)
+        return spectrum
     else:
         print("No spectrum found.")
         return None
 
 def get_flux_and_wl(spectra):
-    spectra[0].writeto("sdss_spectrum.fits", overwrite=True)
-    hdulist = fits.open('sdss_spectrum.fits')
+
+    output_dir = save_spectrum_to_data(spectra[0], "sdss_spectrum.fits")
+    hdulist = fits.open(output_dir)
+    
     data = hdulist[1].data
     header0 = hdulist[0].header
     sdss_z = header0.get('Z', None)
     print(f"SDSS catalog redshift: {sdss_z}")
 
+    #change that badboy to linear
     wavelength = 10**data['loglam']
     flux = data['flux']
     hdulist.close()
+
     return flux, wavelength
 
-'''
-def main():
-    
-        Main function to retrieve and plot SDSS spectrum
-    
-    # Get the spectrum data
-    spectra = get_spectrum(plate=2663, mjd=54234, fiberID=332)
-    flux, wavelength = get_flux_and_wl(spectra)
+def save_spectrum_to_data(spec, filename):
+    this_dir = os.path.dirname(__file__)
+    data_dir = os.path.join(this_dir, '..', 'data')
+    os.makedirs(data_dir, exist_ok=True)
 
-    print(flux, wavelength)
-    # Assume galaxy
-    rest_lines = [6563, 5007, 4861]  # Hα, [O III], Hβ
-
-if __name__ == "__main__":
-    main()
-'''
+    output_path = os.path.join(data_dir, filename)
+    spec.writeto(output_path, overwrite=True)
+    return output_path
